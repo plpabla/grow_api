cf006358055923b91beac96e762faa662bb37235
pawel-laptop <p.blaszczy@gmail.com>
2023-07-27 18:50:04 +0200

    Initial repo

---
+++ src/main/java/plpabla/grow_api/GrowApiApplication.java
@@ -0,0 +1,13 @@
+package plpabla.grow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GrowApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/plpabla/grow_api/GrowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package plpabla.grow_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
